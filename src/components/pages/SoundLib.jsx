9eca3e33cd60a52aa632e0654a66a2309ff8919d
geracia2 <geracia2@gmail.com>
2024-01-02 14:59:50 -0500

    connected relevant api data for sound including audio playback

--- src/components/pages/SoundLib.jsx
+++ src/components/pages/SoundLib.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
-
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 export default function SoundLib() {
   // token for FreeSound
   const fsKey = import.meta.env.VITE_KEY_FS;
 
   const [FSData, setFSData] = useState(null);
+  const [track, setTrack] = useState(null);
   const [input, setInput] = useState(null);
 
   // Preset API requests
@@ -49,6 +51,25 @@ export default function SoundLib() {
     }
   }
 
+  // Selected track API requests
+  async function handleTrack(param) {
+    const url = `https://freesound.org/apiv2/sounds/${param}/`;
+    const options = {
+      headers: {
+        Authorization: `Token ${fsKey}`,
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+      setTrack(data);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
   // update the text input
   function handleChange(e) {
     setInput(e.target.value);
@@ -65,10 +86,18 @@ export default function SoundLib() {
         size="small"
         aria-label="asdf"
       >
-        <Button onClick={() => handlePreset("Dungeon+atmosphere+rpg")}>Dungeon</Button>
+        <Button onClick={() => handlePreset("Dungeon+atmosphere+rpg")}>
+          Dungeon
+        </Button>
         <Button onClick={() => handlePreset("game+battle+loop")}>Battle</Button>
-        <Button onClick={() => handlePreset("fantasy+background+music+rpg+loop")}>Atmosphere</Button>
-        <Button onClick={() => handlePreset("village+music+rpg")}>Village</Button>
+        <Button
+          onClick={() => handlePreset("fantasy+background+music+rpg+loop")}
+        >
+          Atmosphere
+        </Button>
+        <Button onClick={() => handlePreset("village+music+rpg")}>
+          Village
+        </Button>
       </ButtonGroup>
 
       <form onSubmit={handleSubmit}>
@@ -78,18 +107,39 @@ export default function SoundLib() {
 
       {FSData && ( // truthy value, checking to see if you have data before rendering
         <div>
-          <h1>You have FreeSound data</h1>
-          {/* <p>{FSData[0].name}</p> */}
-            {FSData.map((track, i) => (
+          {track && (
+            <>
+              <Typography variant="h6" color="initial">
+                {track.name}
+              </Typography>
+              <p>{track.description}</p>
+              <>
+                tags:{" "}
+                {track.tags.map((tag, i) => (
+                  <span key={i}>{tag}, </span>
+                ))}
+              </>
+              <img src={track.images.waveform_bw_l} alt="" />
+              {console.log(track["previews"]["preview-hq-mp3"])}
+              <audio controls autoplay>
+                <source
+                  src={track["previews"]["preview-hq-mp3"]}
+                  type="audio/ogg"
+                />
+                Your browser does not support the audio element.
+              </audio>
+            </>
+          )}
+          {FSData.map((track, i) => (
             <div key={track.id}>
-              <p>{track.name}</p>
-              <p>{track.username}</p>
-              <p>{track.id}</p>
-              <a href={`https://freesound.org/people/${track.username}/sounds/${track.id}/`}>Link</a>
-
-              
-            </div> 
-          ))} 
+              <p onClick={() => handleTrack(track.id)}>{track.name}</p>
+              <a
+                href={`https://freesound.org/people/${track.username}/sounds/${track.id}/`}
+              >
+                Visit FreeSound Page
+              </a>
+            </div>
+          ))}
         </div>
       )}
     </>
