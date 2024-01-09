1f4ba7be1a63c909a0be561e63987b37b872f7ff
geracia2 <geracia2@gmail.com>
2024-01-08 21:56:15 -0500

    finishing touches on nav

--- src/App.jsx
+++ src/App.jsx
@@ -15,29 +15,16 @@ import Nav from "./components/feature/Nav";
 export default function App() {
   return (
     <>
-      <Container
-        sx={{
-          bgcolor:'background.default', 
-          // bgColor:'background.default',
-          // my: 10,
-          // height:"100vh",
-          // background:
-          //   "linear-gradient(178deg, rgba(40,44,52,1) 70%, rgba(83,63,64,1) 100%)",
-          // backgroundSize: "cover",
-        }}
-        maxWidth="false"
-      >
-        <TopAppBar />
-        {/* <Nav /> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/fabLib" element={<FabLib />} />
-          <Route path="/soundLib" element={<SoundLib />} />
-          <Route path="/scene" element={<Scene />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        {/* <Footer /> */}
-      </Container>
+      <TopAppBar />
+      {/* <Nav /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fabLib" element={<FabLib />} />
+        <Route path="/soundLib" element={<SoundLib />} />
+        <Route path="/scene" element={<Scene />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {/* <Footer /> */}
     </>
   );
 }
